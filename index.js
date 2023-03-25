ba953813bf5ec2958af6690cf8b5c490b67faf84
M-D1 <115486327+M-D1@users.noreply.github.com>
2023-03-25 16:00:24 +0300

    .

---
+++ index.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+  loop: true,
+
+     slidesPerView: 1.4,
+     centeredSlides: true,
+     spaceBetween: 20,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+});

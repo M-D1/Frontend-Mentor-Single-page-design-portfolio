87fde6603c9c6e03d7a837e0d404ba46690eb99b
M-D1 <115486327+M-D1@users.noreply.github.com>
2023-04-06 21:46:37 +0300

    ..

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
+
+
+
+//   const carousel = document.querySelector('.carousel')
+//   const firstImg = carousel.querySelectorAll('img')[0]
+
+
+//   const arrows = document.querySelectorAll('.arrow')
+
+
+
+ 
+// let isDragStart = false
+// let prevPagex ;
+// let  prevScrollLeft;
+
+// let firstImgWidth = firstImg.clientWidth + 32
+
+//  arrows.forEach(arrow => {
+//   arrow.addEventListener('click',function(){
+//     carousel.scrollLeft += arrow.id == 'left' ? -firstImgWidth : firstImgWidth
+//   })
+//  })
+
+// const dragStart =(e)=>{
+//   isDragStart = true
+//   prevPagex = e.pageX
   
-  loop: true,
-
-     slidesPerView: 1.4,
-     centeredSlides: true,
-     spaceBetween: 20,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
+ 
+//   prevScrollLeft = carousel.scrollLeft
   
-});
+  
+// }
+
+//  const dragging = (e)=>{
+//   e.preventDefault()
+//   if(!isDragStart) return
+//   carousel.classList.add('dragging')
+//   let positionDiff = e.pageX - prevPagex
+//   carousel.scrollLeft = prevScrollLeft -positionDiff
+
+//  }
+//  const dragStop = () => {
+//   isDragStart = false
+//   carousel.classList.remove('dragging')
+// }
+
+//  carousel.addEventListener('mousemove',dragging)
+//   carousel.addEventListener('mousedown',dragStart)
+//   carousel.addEventListener('mouseup',dragStop)
+
